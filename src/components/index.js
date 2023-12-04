1e7017c3dfcf16a4ab41044465b585f5822ad4e8
munim <muhammadmunimoff330@gmail.com>
2023-12-04 12:29:51 +0500

    Login, postcard and select

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+import Input from "./Input";
+import Button from "./Button";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
@@ -5,10 +7,13 @@ import Logo from "./Logo";
 import Container from "./container/Container";
 
 
+
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Button
 }
